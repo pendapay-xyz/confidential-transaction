b543f625cbcdec28686afd08e2e91e9b2979f4e5
imduchuyyy <duchuy.124dk@gmail.com>
2024-11-05 17:50:20 +0700

    feat(magicpay): init magic pay

---
+++ contracts/MagicPay.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.4;
+
+contract MagicPay {
+    mapping (bytes32 => address) internal _transactions;
+
+    struct Output {
+        address owner;
+        bytes32 encryptedAmount;
+        bytes message;
+    }
+
+    function pay(bytes32[] calldata inputs, Output[] calldata outputs, uint256 outAmount) public payable {
+        for (uint256 i = 0; i < inputs.length; i++) {
+            require(_transactions[inputs[i]] == msg.sender, "Invalid transaction");
+            delete _transactions[inputs[i]];
+        }
+
+        for (uint256 i = 0; i < outputs.length; i++) {
+            require(_transactions[outputs[i].encryptedAmount] == address(0), "Existed transaction");
+            _transactions[outputs[i].encryptedAmount] = outputs[i].owner;
+        }
+
+        if (outAmount > 0) {
+            (bool success, ) = msg.sender.call{value: outAmount}("");
+            require(success, "Transfer failed.");
+        }
+    }
+}
