4155d8728e104bf33776a8ccea4dd5df773d248a
imduchuyyy <duchuy.124dk@gmail.com>
2025-05-14 10:48:19 +0700

    feat: remove Lock contract and add Profile contract

---
+++ contracts/Profile.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.4;
+
+contract Profile {
+    mapping(address => mapping(bytes32 => bytes)) private _profiles;
+
+    event ProfileUpdated(address indexed user, bytes32 indexed key, bytes value);
+
+    function setProfile(bytes32 key, bytes memory value) public {
+        _profiles[msg.sender][key] = value;
+        emit ProfileUpdated(msg.sender, key, value);
+    }
+}
